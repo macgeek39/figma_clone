f32947fe9b590c58a9275efbe4ff0ca668b18033
Taiwei Ko <152499488+macgeek39@users.noreply.github.com>
2024-02-19 03:49:50 -0600

    Added Room tsx

---
+++ web_builder/app/Room.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import { RoomProvider } from "./liveblocks.config";
+import { ClientSideSuspense } from "@liveblocks/react";
+
+export function Room({ children }: { children: ReactNode}){
+    return(
+        <RoomProvider id="my-room" initialPresence={{}}>
+            <ClientSideSuspense fallback={<div>Loading...</div>}>
+                {()=> children}
+            </ClientSideSuspense>
+        </RoomProvider>
+    );
+}
